e80322698ad7c1c9de633016d26c7795cafd2b42
DanDev-rs <rudyh.rdr@yandex.ru>
2019-08-23 12:02:53 +0800

    second push

--- lib/main.dart
+++ lib/main.dart
@@ -6,43 +6,51 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: RedPage(),
+      theme: ThemeData(primarySwatch: Colors.teal),
+      home: PageOne(),
     );
   }
 }
 
-class RedPage extends StatelessWidget {
-  int page = 1;
+
+class PageOne extends StatefulWidget {
+  @override
+  _PageOne createState() => _PageOne();
+}
+class _PageOne extends State<PageOne> {
   @override
   Widget build(BuildContext context) {
+    bool isMobile = MediaQuery.of(context).size.width < 600;
     return Scaffold(
-      drawer: Drawer(
+      appBar: isMobile ? AppBar() : null,
+      drawer: isMobile ? Drawer(
           elevation: 0,
           child: Column(
             children: <Widget>[
               FlatButton(
-                child: Text("Red"),
-                color: (Colors.red),
+                child: Text("P1"),
+                color: (Colors.tealAccent[50]),
                 onPressed: () {
                   Navigator.pop(context);
                 },
               ),
               FlatButton(
-                  child: Text("Blue"),
-                  color: (Colors.blue),
+                  child: Text("P2"),
+                  color: (Colors.indigoAccent[100]),
                   onPressed: () {
-                    Navigator.push(context ,MaterialPageRoute(builder: (context) => BluePage()));
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => PageTwo()));
                   })
             ],
           )),
       body: FlatButton(
-        child: Text("!"),
+        child: Text("Info"),
         onPressed: () {
           showDialog(
               context: context,
               builder: (context) {
                 return AlertDialog(
-                  title: Text("Notification"),
+                  title: Text("You are on page №1"),
                   actions: <Widget>[
                     FlatButton(
                       child: Text("Ok"),
@@ -59,11 +67,48 @@ class RedPage extends StatelessWidget {
   }
 }
 
-class BluePage extends StatelessWidget {
+class PageTwo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
-    );
+        drawer: Drawer(
+            elevation: 0,
+            child: Column(
+              children: <Widget>[
+                FlatButton(
+                  child: Text("P1"),
+                  color: (Colors.tealAccent[50]),
+                  onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => PageOne()));
+                  },
+                ),
+                FlatButton(
+                    child: Text("P2"),
+                    color: (Colors.indigoAccent[100]),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    })
+              ],
+            )),
+        body: FlatButton(
+            child: Text("Info"),
+            onPressed: () {
+              showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: Text("You are on page №2"),
+                      actions: <Widget>[
+                        FlatButton(
+                          child: Text("Ok"),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        )
+                      ],
+                    );
+                  });
+            }));
   }
 }
